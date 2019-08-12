3d3a090e3005365f9bad79a6c4d3ea4785923cf8
AbnerChen <cwc0277777@gmail.com>
2019-08-13 01:09:38 +0800

    update LocalDB ini file

--- CRUDporject/CRUDproject.Model/Migrations/201908070511565_ini.cs
+++ CRUDporject/CRUDproject.Model/Migrations/201908070511565_ini.cs
@@ -7,13 +7,21 @@ namespace CRUDproject.Model.Migrations
     {
         public override void Up()
         {
-
-            
+            CreateTable(
+                "dbo.UserProfile",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    Name = c.String(maxLength: 25),
+                    Gender = c.Int(nullable: false),
+                    Birthday = c.String(maxLength: 20),
+                })
+                .PrimaryKey(t => t.Id);
         }
-        
+
         public override void Down()
         {
-     
+            DropTable("dbo.UserProfile");
         }
     }
 }
